182f30fba1afb1c9639f6116dadea953db2238bd
Rodrigo <rodrigo.a42@gmail.com>
2024-03-14 12:56:26 -0300

    aula2_homework

--- src/app/cadastro/page.tsx
+++ src/app/cadastro/page.tsx
@@ -15,7 +15,7 @@ function Cadastro() {
             htmlFor="floating_email"
             className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
           >
-            Email address
+            Email
           </label>
         </div>
         <div className="relative z-0 w-full mb-5 group">
@@ -31,7 +31,7 @@ function Cadastro() {
             htmlFor="floating_password"
             className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
           >
-            Password
+            Senha
           </label>
         </div>
         <div className="relative z-0 w-full mb-5 group">
@@ -47,7 +47,7 @@ function Cadastro() {
             htmlFor="floating_repeat_password"
             className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
           >
-            Confirm password
+            Confirmar senha
           </label>
         </div>
         <div className="grid md:grid-cols-2 md:gap-6">
@@ -64,7 +64,7 @@ function Cadastro() {
               htmlFor="floating_first_name"
               className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
-              First name
+              Primeiro nome
             </label>
           </div>
           <div className="relative z-0 w-full mb-5 group">
@@ -80,7 +80,7 @@ function Cadastro() {
               htmlFor="floating_last_name"
               className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
-              Last name
+              Sobrenome
             </label>
           </div>
         </div>
@@ -99,7 +99,7 @@ function Cadastro() {
               htmlFor="floating_phone"
               className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
-              Phone number (123-456-7890)
+              Número de telefone
             </label>
           </div>
           <div className="relative z-0 w-full mb-5 group">
@@ -115,7 +115,7 @@ function Cadastro() {
               htmlFor="floating_company"
               className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
-              Company (Ex. Google)
+              Endereço
             </label>
           </div>
         </div>
@@ -123,7 +123,7 @@ function Cadastro() {
           type="submit"
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
-          Submit
+          Cadastrar
         </button>
       </form>
     </div>
--- src/app/components/Titulo.tsx
+++ src/app/components/Titulo.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { VscAccount } from "react-icons/vsc";
+import { BsPersonFillAdd } from "react-icons/bs";
 
 function Titulo() {
   return (
@@ -60,7 +61,7 @@ function Titulo() {
                   className="block py-2 px-3 md:p-0 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent"
                   aria-current="page"
                 >
-                  <VscAccount className="inline pe-1" />
+                  <BsPersonFillAdd className="inline pe-1" />
                   Cadastrar-se
                 </Link>
               </li>
